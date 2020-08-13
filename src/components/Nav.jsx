389834f066c810f58582676ad48946858dd582ef
Diego Valdez <dgogo9@hotmail.com>
2020-08-13 11:31:19 -0500

    ReactJs Clock first version

---
+++ src/components/Nav.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../assets/styles/Nav.styl";
+
+const Nav = () => {
+  return (
+    <div className="Nav">
+      <h1>ReactJs Clock</h1>
+    </div>
+  );
+};
+
+export default Nav;
